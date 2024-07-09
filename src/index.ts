0d1b0554e6f30fabeb038d3c5555ae48fb3a1e7e
Terry Gonguet <terry@gonguet.com>
2024-07-09 02:34:10 +0200

    feat: :sparkles: Add more utility objects to `stateMachine`'s returned object

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { writable, type Readable } from "svelte/store"
+import { writable, readonly, type Readable, derived } from "svelte/store"
 
 type StateMachine<
 	State extends { type: string },
@@ -15,29 +15,61 @@ type StateMachine<
 	}
 }
 
+type Is<State extends { type: string }> = {
+	[StateType in State["type"]]: Readable<boolean>
+}
+
 export function stateMachine<
 	State extends { type: string },
 	Action extends { type: string },
 >(
 	initialState: State,
 	machine: StateMachine<State, Action>,
-): Readable<State> & { dispatch(action: Action): void } {
-	let { subscribe, update, set } = writable(initialState)
+): {
+	state: Readable<State>
+	dispatch(action: Action): void
+	transitioning: Readable<boolean>
+	is: Is<State>
+} {
+	let state = writable(initialState)
+
+	let transitioning = writable(false)
+	let isLocked = false
+	transitioning.subscribe(value => (isLocked = value))
+
+	function dispatch($action: Action) {
+		if (isLocked) return
+		state.update($state => {
+			let reducer =
+				machine[$state.type as State["type"]]?.[
+					$action.type as Action["type"]
+				]
+			let next = reducer?.($state, $action) ?? $state
+			if (next instanceof Promise) {
+				transitioning.set(true)
+				next.then(
+					newState => {
+						state.set(newState)
+						transitioning.set(false)
+					},
+					() => transitioning.set(false),
+				)
+				return $state
+			} else return next
+		})
+	}
+
+	let is = Object.fromEntries(
+		Object.keys(machine).map(type => [
+			type,
+			derived(state, $state => $state.type == type),
+		]),
+	) as Is<State>
 
 	return {
-		subscribe,
-		dispatch(action: Action) {
-			update(state => {
-				let reducer =
-					machine[state.type as State["type"]]?.[
-						action.type as Action["type"]
-					]
-				let next = reducer?.(state, action) ?? state
-				if (next instanceof Promise) {
-					next.then(set)
-					return state
-				} else return next
-			})
-		},
+		state: readonly(state),
+		transitioning: readonly(transitioning),
+		dispatch,
+		is,
 	}
 }
